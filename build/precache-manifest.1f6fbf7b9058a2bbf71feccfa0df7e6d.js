6fe464fb66653f8dfe5853c561368ed45acb2e74
liudanqing <1278476591@qq.com>
2019-11-15 10:46:17 +0800

    add

---
+++ build/precache-manifest.1f6fbf7b9058a2bbf71feccfa0df7e6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e34d48bbe532a138fdb9b84f9e4c4091",
+    "url": "index.html"
+  },
+  {
+    "revision": "084c9f8901c5c3c323a2",
+    "url": "static/css/2.59c644fd.chunk.css"
+  },
+  {
+    "revision": "a03f58e8ffd492dabb30",
+    "url": "static/css/main.0378a9cf.chunk.css"
+  },
+  {
+    "revision": "084c9f8901c5c3c323a2",
+    "url": "static/js/2.8ae7d6ed.chunk.js"
+  },
+  {
+    "revision": "a03f58e8ffd492dabb30",
+    "url": "static/js/main.90f78139.chunk.js"
+  },
+  {
+    "revision": "067790caabdba6a1564e",
+    "url": "static/js/runtime-main.d10b7575.js"
+  }
+]);
